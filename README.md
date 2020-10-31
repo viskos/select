456dd2e9e568e387945fe97ab8d5e267e22b8134
viskos <viskoos.fd@gmail.com>
2020-10-31 23:40:55 +0300

    docs: edit README.md, add comments

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-select
+<h1>Select plugin</h1>
+<h3>For start project use "Parcel" npm i parcel -g</h3>
+<p>Run project: parcel index.html</p>
+
+<h3>Plugin public methods:</h3>
+<p>global - s</p>
+<ul>
+    <li>s.open() - open select</li>
+    <li>s.close() - close select</li>
+    <li>s.destroy() - destroy select</li>
+    <li>s.toggle() - open/close select</li>
+    <li>s.select(id) - id === string. Choose current element</li>
+    <li>s.selectedId - get current element id</li>
+</ul>
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import {Select} from './select/select'
 import './select/style.scss'
 
 const select = new Select('#select', {
+    //custom placeholder
     placeholder: 'Select element',
+    //selected element
     selectedId: '1',
+    //options
     data: [
         {id: '1', value: 'Element one'},
         {id: '2', value: 'Element two'},
@@ -12,6 +15,7 @@ const select = new Select('#select', {
         {id: '5', value: 'Element five'},
         {id: '6', value: 'Element six'}
     ],
+    //callback
     onSelect(item) {
         console.log('selected item: ', item)
     }
